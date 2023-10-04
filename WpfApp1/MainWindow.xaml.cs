7476f6a28dc2c55007b6dc72a7eb965dfa1430f2
yfrkuo <yfrkuo@M193>
2023-10-04 10:31:00 +0200

    added counter

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,70 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Settings : INotifyPropertyChanged
+    {
+        //public string Url = "https://s3.amazonaws.com/creare-websites-wpms-legacy/wp-content/uploads/sites/32/2016/03/01200959/canstockphoto22402523-arcos-creator.com_-1024x1024.jpg"
+        public Settings()
+        {
+            this.Url = "https://s3.amazonaws.com/creare-websites-wpms-legacy/wp-content/uploads/sites/32/2016/03/01200959/canstockphoto22402523-arcos-creator.com_-1024x1024.jpg";
+            this.Counter = "ABC";
+        }
+
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                if (value != _url)
+                {
+                    _url = value;
+                    NotifyPropertyChanged("Url");
+                }
+            }
+        }
+
+        private string _counter;
+        public string Counter
+        {
+            get { return _counter; }
+            set
+            {
+                if (value != _counter)
+                {
+                    _counter = value;
+                    NotifyPropertyChanged("Counter");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
     public partial class MainWindow : Window
     {
+        Settings Settings;
         public MainWindow()
         {
+            this.Settings = new Settings();
             InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void aPicture_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Sett
         }
     }
 }
